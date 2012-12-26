65c5a323b471767dc5bb10226f7fca982668b6ca
Alex Crichton <alex@alexcrichton.com>
2012-12-26 16:15:00 -0600

    Adding an initchecker

--- rust/analysis/initcheck.rs
+++ rust/analysis/initcheck.rs
@@ -13,9 +13,75 @@ pub fn check(a : &Program) {
 
 impl Initchecker {
   fn check(a : &Program) {
-    ignore(a);
-    /*for a.decls().each |x| {*/
-    /*  self.ic_gdecl(*x)*/
-    /*}*/
+    for a.decls.each |x| {
+      match *x {
+        @Function(_, _, _, body) => self.analyze(body),
+        _ => ()
+      }
+    }
+  }
+
+  fn analyze(s : @Statement) {
+    match s {
+      @Markeds(ref m) => self.err.with(m, |x| self.analyze(x)),
+      @If(_, s1, s2) | @Seq(s1, s2) => { self.analyze(s1); self.analyze(s2); },
+      @While(_, s) => self.analyze(s),
+      @Declare(id, _, s) =>
+        if !self.live(id, s) {
+          self.analyze(s);
+        } else {
+          self.err.add(fmt!("Uninitialized variable: '%s'", id.val));
+        },
+      @For(_, _, _, body) => self.analyze(body),
+      _ => ()
+    }
+  }
+
+  fn live(sym : @Symbol, s : @Statement) -> bool {
+    match s {
+      @Declare(id, _, s) => id != sym && self.live(sym, s),
+      @Assign(e1, Some(_), e2) => self.uses(sym, e1) || self.uses(sym, e2),
+      @Assign(_, _, e2) => self.uses(sym, e2),
+      @If(e, s1, s2) =>
+        self.uses(sym, e) || self.live(sym, s1) || self.live(sym, s2),
+      @While(e, s) => self.uses(sym, e) || self.live(sym, s),
+      @Break | @Continue | @Nop => false,
+      @Express(e) | @Return(e) => self.uses(sym, e),
+      @Seq(s1, s2) =>
+        self.live(sym, s1) || (self.live(sym, s2) && !self.defines(sym, s1)),
+      @Markeds(ref m) => self.live(sym, m.data),
+      @For(s1, e, s2, s3) =>
+        self.live(sym, s1) || self.uses(sym, e) || self.live(sym, s2) ||
+        self.live(sym, s3)
+    }
+  }
+
+  fn uses(sym : @Symbol, e : @Expression) -> bool {
+    match e {
+      @Marked(ref m) => self.uses(sym, m.data),
+      @Var(id) => id == sym,
+      @UnaryOp(_, e) | @Field(e, _) | @Deref(e) | @AllocArray(_, e) =>
+        self.uses(sym, e),
+      @BinaryOp(_, e1, e2) | @ArrSub(e1, e2) =>
+        self.uses(sym, e1) || self.uses(sym, e2),
+      @Ternary(e1, e2, e3) =>
+        self.uses(sym, e1) || self.uses(sym, e2) || self.uses(sym, e3),
+      @Call(_, ref args) => args.any(|x| self.uses(sym, *x)),
+      _ => false
+    }
+  }
+
+  fn defines(sym : @Symbol, s : @Statement) -> bool {
+    match s {
+      @Declare(_, _, s) => self.defines(sym, s),
+      @Assign(@Marked(ref m), op, e2) =>
+        self.defines(sym, @Assign(m.data, op, e2)),
+      @Assign(@Var(id), _, _) => id == sym,
+      @If(_, s1, s2) => self.defines(sym, s1) && self.defines(sym, s2),
+      @While(_,_) | @Assign(_,_,_) | @For(_,_,_,_) | @Nop | @Express(_) => false,
+      @Break | @Continue | @Return(_) => true,
+      @Seq(s1, s2) => self.defines(sym, s1) || self.defines(sym, s2),
+      @Markeds(ref m) => self.defines(sym, m.data)
+    }
   }
 }
