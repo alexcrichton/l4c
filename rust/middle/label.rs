931455466004268380b8fab2be35667e16b55576
Alex Crichton <alex@alexcrichton.com>
2012-12-28 01:55:14 -0600

    On osx, add a '_' to labels, otherwise don't

--- rust/middle/label.rs
+++ rust/middle/label.rs
@@ -5,11 +5,17 @@ pub enum Label {
   Internal(~str)
 }
 
+#[cfg(target_os = "macos")]
+fn prefix() -> ~str { ~"_" }
+
+#[cfg(target_os = "linux")]
+fn prefix() -> ~str { ~"" }
+
 impl Label : PrettyPrint {
   fn pp() -> ~str {
     match self {
-      External(ref s) => ~"_" + *s,
-      Internal(ref s) => ~"_" + ~"_c0_" + *s
+      External(ref s) => prefix() + *s,
+      Internal(ref s) => prefix() + ~"_c0_" + *s
     }
   }
 }
