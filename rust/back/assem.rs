1f87244fc5fd946e6833dfd31a94492b749a4a21
Alex Crichton <alex@alexcrichton.com>
2012-12-30 17:51:27 -0600

    Fix another instance of 'jmp' => 'j'

--- rust/back/assem.rs
+++ rust/back/assem.rs
@@ -382,7 +382,7 @@ impl Function {
                 (_, ir::False) => {
                   skipped.push(tid);
                   skipped.push(fid);
-                  out.write_str(fmt!("  jmp%s %s\n", cond.suffix(), lbl(fid)));
+                  out.write_str(fmt!("  j%s %s\n", cond.suffix(), lbl(fid)));
                 }
                 _ => fail(~"invalidly specified edges")
               }
