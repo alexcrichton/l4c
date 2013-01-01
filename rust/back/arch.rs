a6e195bfc975afbcd97c35bf45dcff1062cf4762
Alex Crichton <alex@alexcrichton.com>
2013-01-01 04:38:00 -0600

    Must have forgot these at some point...

--- rust/back/arch.rs
+++ rust/back/arch.rs
@@ -6,6 +6,7 @@ pub const arg_regs : uint = 6;
 pub const ptrsize : uint = 8;
 pub const intsize : uint = 4;
 pub const ret_reg : Register = EAX;
+pub const num_regs : uint = 13;
 
 pub fn arg_reg(i : uint) -> Register {
   match i {
