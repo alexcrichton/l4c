6161d01375c968e8e745b5a566e286a968dae677
Alex Crichton <alex@alexcrichton.com>
2013-01-25 14:01:11 -0500

    Finally get the graph to compile, progress!

--- rust/utils/graph.rs
+++ rust/utils/graph.rs
@@ -1,16 +1,15 @@
 use core::hashmap::linear::{LinearMap, LinearSet};
 
 use io::WriterUtil;
-use utils::set;
 
 pub type NodeId = uint;
 pub type NodeSet = LinearSet<NodeId>;
 
 pub struct Graph<N, E> {
-  priv nodes : LinearMap<NodeId, N>,
-  priv succ  : LinearMap<NodeId, LinearMap<NodeId, E>>,
-  priv pred  : LinearMap<NodeId, NodeSet>,
-  priv next : NodeId
+  priv nodes: LinearMap<NodeId, N>,
+  priv succ:  LinearMap<NodeId, ~mut LinearMap<NodeId, E>>,
+  priv pred:  LinearMap<NodeId, ~mut NodeSet>,
+  priv next:  NodeId
 }
 
 pub fn Graph<N : Copy, E : Copy>() -> Graph<N, E>{
@@ -34,8 +33,8 @@ impl<N : Copy, E : Copy> Graph<N, E> {
   fn new_id(&mut self) -> NodeId {
     let ret = self.next;
     self.next += 1;
-    self.succ.insert(ret, LinearMap::new());
-    self.pred.insert(ret, LinearSet::new());
+    self.succ.insert(ret, ~mut LinearMap::new());
+    self.pred.insert(ret, ~mut LinearSet::new());
     ret
   }
 
@@ -44,7 +43,8 @@ impl<N : Copy, E : Copy> Graph<N, E> {
   }
 
   pure fn edge(&self, a: NodeId, b: NodeId) -> E {
-    *self.succ.get(&a).get(&b)
+    let a_succ = self.succ.get(&a);
+    return *a_succ.get(&b);
   }
 
   fn add_node(&mut self, id: NodeId, n: N) {
@@ -61,14 +61,14 @@ impl<N : Copy, E : Copy> Graph<N, E> {
 
   fn remove_node(&mut self, n: NodeId) {
     assert self.nodes.remove(&n);
-    for self.succ.get(&n).each_key |k| {
+    let succ = self.succ.pop(&n).unwrap();
+    let pred = self.pred.pop(&n).unwrap();
+    for succ.each_key |k| {
       assert self.pred.get(k).remove(&n)
     }
-    for self.pred.get(&n).each |k| {
+    for pred.each |k| {
       assert self.succ.get(k).remove(&n);
     }
-    assert self.succ.remove(&n);
-    assert self.pred.remove(&n);
   }
 
   fn remove_edge(&mut self, n1: NodeId, n2: NodeId) -> E {
@@ -83,7 +83,8 @@ impl<N : Copy, E : Copy> Graph<N, E> {
 
   pure fn each_edge(&self, f: fn(NodeId, NodeId) -> bool) {
     for self.succ.each |&a, map| {
-      map.each_key(|&b| f(a, b));
+      /* TODO: this shouldn't be unsafe */
+      unsafe { map.each_key(|&b| f(a, b)); }
     }
   }
 
@@ -92,15 +93,21 @@ impl<N : Copy, E : Copy> Graph<N, E> {
   }
 
   pure fn each_pred(&self, n: NodeId, f: fn(NodeId) -> bool) {
-    self.pred.get(&n).each(|&k| f(k));
+    let preds = self.pred.get(&n);
+    /* TODO: this shouldn't be unsafe */
+    unsafe { preds.each(|&k| f(k)); }
   }
 
   pure fn each_succ(&self, n: NodeId, f: fn(NodeId) -> bool) {
-    self.succ.get(&n).each_key(|&k| f(k));
+    let succ = self.succ.get(&n);
+    /* TODO: this shouldn't be unsafe */
+    unsafe { succ.each_key(|&k| f(k)); }
   }
 
   pure fn each_succ_edge(&self, n: NodeId, f: fn(NodeId, E) -> bool) {
-    self.succ.get(&n).each(|&n, &e| f(n, e));
+    let succ = self.succ.get(&n);
+    /* TODO: this shouldn't be unsafe */
+    unsafe { succ.each(|&n, &e| f(n, e)); }
   }
 
   fn each_postorder(&self, root: NodeId, f: fn(&NodeId) -> bool) {
@@ -113,24 +120,39 @@ impl<N : Copy, E : Copy> Graph<N, E> {
     vec::rev_each(order, f);
   }
 
-  fn map<N2 : Copy, E2 : Copy>(n : fn(NodeId, &N) -> N2,
-                               e : fn(&E) -> E2) -> Graph<N2, E2> {
-    let g2 = Graph();
+  fn map_nodes(&mut self, f: fn(NodeId, N) -> N) {
+    let mut keys = ~[];
+    for self.nodes.each_key |&k| { keys.push(k); }
+    for keys.each |&k| {
+      let new = f(k, *self.nodes.get(&k));
+      self.nodes.insert(k, new);
+    }
+  }
+
+  fn map<N2 : Copy, E2 : Copy>(n: fn(NodeId, &N) -> N2,
+                               e: fn(&E) -> E2) -> Graph<N2, E2> {
+    let mut g2 = Graph();
     g2.next = self.next;
     for self.nodes.each |&k, v| {
       g2.nodes.insert(k, n(k, v));
     }
     for self.pred.each |&k, v| {
-      let set = LinearSet::new();
-      for v.each |&value| {
-        set.insert(value);
+      let set = ~mut LinearSet::new();
+      /* TODO: this shouldn't be unsafe */
+      unsafe {
+        for v.each |&value| {
+          set.insert(value);
+        }
       }
       g2.pred.insert(k, set);
     }
     for self.succ.each |&k, v| {
-      let map = LinearMap::new();
-      for v.each |&k, v| {
-        map.insert(k, e(v));
+      let map = ~mut LinearMap::new();
+      /* TODO: this shouldn't be unsafe */
+      unsafe {
+        for v.each |&k, v| {
+          map.insert(k, e(v));
+        }
       }
       g2.succ.insert(k, map);
     }
@@ -148,18 +170,21 @@ impl<N : Copy, E : Copy> Graph<N, E> {
       out.write_str(~"];\n");
     }
     for self.succ.each |&id1, neighbors| {
-      for neighbors.each |&id2, e| {
-        out.write_str(nid(id1));
-        out.write_str(~" -> ");
-        out.write_str(nid(id2));
-        out.write_str(~" [");
-        out.write_str(edge(e));
-        out.write_str(~"];\n");
+      /* TODO: this shouldn't be unsafe */
+      unsafe {
+        for neighbors.each |&id2, e| {
+          out.write_str(nid(id1));
+          out.write_str(~" -> ");
+          out.write_str(nid(id2));
+          out.write_str(~" [");
+          out.write_str(edge(e));
+          out.write_str(~"];\n");
+        }
       }
     }
   }
 
-  fn postorder(root : NodeId) -> (~[NodeId], LinearMap<NodeId, int>) {
+  fn postorder(&self, root : NodeId) -> (~[NodeId], LinearMap<NodeId, int>) {
     let mut ordering = LinearMap::new();
     self.traverse(&mut ordering, root, 0);
     let mut v = ~[];
@@ -176,8 +201,12 @@ impl<N : Copy, E : Copy> Graph<N, E> {
       None => {
         o.insert(n, -1);
         let mut next = i;
-        for self.succ.get(&n).each |&id, _| {
-          next = self.traverse(o, id, next);
+        let succ = self.succ.get(&n);
+        /* TODO: this shouldn't be unsafe */
+        unsafe {
+          for succ.each |&id, _| {
+            next = self.traverse(&mut *o, id, next);
+          }
         }
         o.insert(n, next);
         return next + 1;
@@ -194,7 +223,7 @@ impl<N : Copy, E : Copy> Graph<N, E> : Index<NodeId, N> {
 
 #[test]
 fn test_basic() {
-  let g = Graph();
+  let mut g = Graph();
   let (n1, n2) = (g.new_id(), g.new_id());
   g.add_node(n1, 1);
   g.add_node(n2, 2);
@@ -216,14 +245,14 @@ fn test_basic() {
 #[test]
 #[should_fail]
 fn test_random_id_fails() {
-  let g = Graph::<int, int>();
+  let mut g = Graph::<int, int>();
   g.add_node(1, 1);
 }
 
 #[test]
 #[should_fail]
 fn test_same_id_fails() {
-  let g = Graph::<int, int>();
+  let mut g = Graph::<int, int>();
   let id = g.new_id();
   g.add_node(id, 1);
   g.add_node(id, 1);
